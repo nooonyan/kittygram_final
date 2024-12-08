98c0168117ccad372d6f940a0901e4a8cedbefe0
nooonyan <ivan.gorsky.04@gmail.com>
2024-12-09 02:44:52 +0500

    fix pep8

--- backend/cats/tests.py
+++ backend/cats/tests.py
@@ -4,10 +4,11 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+User = get_user_model()
+
 
 class CatsAPITestCase(TestCase):
     def setUp(self):
-        User = get_user_model()
         self.user = User.objects.create_user(username='auth_user')
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
